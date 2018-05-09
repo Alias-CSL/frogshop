a0e1b38757c9ce82defb39c519c031e1b0cfd2fb
ChenShengLi <838930725@qq.com>
2018-05-09 10:45:13 +0800

    添加异常处理类

--- src/main/java/cn/barathrum/frogshop/exception/DefaultExceptionHandler.java
+++ src/main/java/cn/barathrum/frogshop/exception/DefaultExceptionHandler.java
@@ -6,13 +6,18 @@ import org.apache.shiro.authz.UnauthenticatedException;
 import org.apache.shiro.authz.UnauthorizedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.util.NestedServletException;
 
 @ControllerAdvice
 public class DefaultExceptionHandler {
 	
 	@ExceptionHandler(value={UnknownAccountException.class,UnauthorizedException.class,UnauthenticatedException.class,AuthorizationException.class})
 	public String loginExceptionHandler() {
-		return "home/login";
+		return "redirect:/login.html";
 	}
 	
+	@ExceptionHandler(value={NestedServletException.class})
+	public String handlerServletException() {
+		return "404.jsp";
+	}
 }
--- src/main/webapp/WEB-INF/views/background/login.jsp
+++ src/main/webapp/WEB-INF/views/background/login.jsp
@@ -8,7 +8,7 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>LarryCMS后台登录</title>
+<title>FrogShop后台登录</title>
 <meta name="renderer" content="webkit">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport"
